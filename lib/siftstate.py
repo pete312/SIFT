99b65ec405e7d3d8dedd06401aecd6cd64d90959
Peter Moore <1petemoore@gmail.com>
2012-02-22 02:19:19 -0600

    More functionality in state machine.

--- lib/siftstate.py
+++ lib/siftstate.py
@@ -8,18 +8,47 @@ class SiftState(object):
     def __init__(self):
         self._states = []
         self._current_state = None
+        self._reset_at_top_state = False
 
     def version(self):
         return siftproperty.version()
+        
+    def next_state(self):
+        if self._current_state == None:
+            raise Exception("Uninitialized", "State not initialized")
+            
+        if self._current_state < len(self._states):
+            self._current_state += 1
+        else:
+            if self._reset_at_top_state:
+                self._current_state = 0
+        return self._states[self._current_state]
+                
+                
+    def prev_state(self):
+        if self._current_state == None:
+            raise Exception("Uninitialized", "State not initialized")
+            
+        if self._current_state > len(self._states):
+            self._current_state += 1
+        else:
+            if self._reset_at_top_state:
+                self._current_state = 0
+        return self._states[self._current_state]
        
        
     @abc.abstractmethod
-    def onEnter(self):
+    def on_enter(self):
         pass
         
     @abc.abstractmethod
-    def onLeave(self):
+    def on_leave(self):
         pass
+    
+    def set_state(self, desired_state):
+        """set to desired state"""
+        if self._current_state == None:
+            raise Exception("Uninitialized", "State not initialized")
         
     def init_states(self, state_list ):
         self._states = state_list
@@ -30,5 +59,6 @@ class SiftState(object):
         
     def del_state(self):
         del self._states
+        self._current_state = None
         
     state = property(get_state, init_states, del_state, "State register.")
--- test/test_SiftState.py
+++ test/test_SiftState.py
@@ -5,11 +5,11 @@ import siftproperty
 
 class MyStateMachine(siftstate.SiftState):
     
-    def onEnter(self):
-        return True
+    def on_enter(self):
+        return "enter"
         
-    def onLeave(self):
-        return True
+    def on_leave(self):
+        return "leave"
 
      
 class TestSiftState(unittest.TestCase):
@@ -19,23 +19,63 @@ class TestSiftState(unittest.TestCase):
         self.traffic_states = ["green","amber","red"]
         self.alert_states = ["good","bad","downright ugly"]
         
-    def test_basic_inheritence(self):
+    def test1_basic_inheritence(self):
         self.assertTrue( self.sift_state )
         self.assertEqual( siftproperty.version() , self.sift_state.version() )
             
-    def test_register_states(self):
+    def test2_register_states(self):
+        """assert init_state registers user states"""
         my_states = [1,2,3,4]
         self.sift_state.init_states(my_states)
         
-        self.assertEqual(my_states[0] , self.sift_state.get_state() )
+        self.assertEqual(my_states[0], self.sift_state.get_state() )
         
-    def test_state_change_up(self):
+    def test2_unregistered_operations(self):
+        """Assert raise exception when state is not initialized"""
+        self.failUnlessRaises ( Exception ,self.sift_state.next_state )
+        self.failUnlessRaises ( Exception ,self.sift_state.prev_state )
+        self.failUnlessRaises ( Exception ,self.sift_state.set_state, "dummy" )
+    
+    def test3_state_change_to_desired_state(self):
+        """Set state to specific state"""
+        test_obj = self.sift_state
+        test_obj.init_states(self.alert_states)
+        self.assertEqual(self.alert_states[0] , test_obj.get_state() )
+        
+        test_obj.set_state(self.alert_states[2])
+        self.assertEqual(self.alert_states[2], test_obj.get_state())
+        
+    def test4_state_change_up(self):
+        """State change from green to amber to red"""
         test_obj = self.sift_state
         test_obj.init_states(self.traffic_states)
+        self.assertEqual(self.traffic_states[0] , test_obj.get_state() )
+        self.assertEqual(self.traffic_states[1] , test_obj.next_state() ) 
+        self.assertEqual(self.traffic_states[1] , test_obj.get_state() )
+        self.assertEqual(self.traffic_states[2] , test_obj.next_state() ) 
+        self.assertEqual(self.traffic_states[2] , test_obj.get_state() ) 
         
-    def test_state_change_dn(self):
+
+    def test4_state_change_dn(self):
+        """State change from bad to good"""
+        
+        my_states = self.alert_states
         test_obj = self.sift_state
-        test_obj.init_states(self.traffic_states)
+        test_obj.init_states(self.alert_states)
+        
+        test_obj.set_state(my_states[2])
+        self.assertEqual(my_states[2] , test_obj.get_state() )
+        self.assertEqual(my_states[1] , test_obj.prev_state() ) 
+        self.assertEqual(my_states[1] , test_obj.get_state() )
+        self.assertEqual(my_states[0] , test_obj.prev_state() ) 
+        self.assertEqual(my_states[0] , test_obj.get_state() )
+        
+
+            
+        
+    def test5_state_change_boundries(self):
+        """ Test that Red changes to Green """
+        self.assertTrue(False, "TODO: Not yet tested") 
         
     @classmethod
     def state_up_callback():
